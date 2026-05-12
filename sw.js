00e688707b01cf924c140f042b17b8ad5de01dc3
Cecil2025t <xishangbin@gmail.com>
2026-05-12 10:20:53 +0800

    Fix manifest icons, add id field, optimize service worker with all icons

--- sw.js
+++ sw.js
@@ -3,7 +3,15 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon.svg'
+  '/icon.svg',
+  '/icon-72.png',
+  '/icon-96.png',
+  '/icon-128.png',
+  '/icon-144.png',
+  '/icon-152.png',
+  '/icon-192.png',
+  '/icon-384.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
